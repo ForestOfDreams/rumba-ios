429b5d489d1c7e814b83802ee4498674bb079491
Владислав Щукин <dorry@DorryBook.local>
2021-12-30 02:07:12 +0300

    init

---
+++ Rumba/RumbaApp.swift
@@ -0,0 +1,17 @@
+//
+//  RumbaApp.swift
+//  Rumba
+//
+//  Created by Владислав Щукин on 30.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct RumbaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
